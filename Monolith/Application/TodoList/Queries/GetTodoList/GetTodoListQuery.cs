4475135b76586511fb17001236880cc4f2d56590
khoabui <buidangkhoa05@gmail.com>
2024-05-18 15:06:53 +0700

    somethings

---
+++ Monolith/Application/TodoList/Queries/GetTodoList/GetTodoListQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.TodoList.Queries.GetTodoList
+{
+    public class GetTodoListQuery
+    {
+
+    }
+}
